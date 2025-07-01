36b77b5d1bd3539a16e51d7cc941e61a97770f90
yoni290 <yfisuhu@gmail.com>
2025-07-01 19:46:44 +0300

    Apply modulus on value of hash code on each iteration

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class HashMap {
-    load_factor = null
-    capacity = null
+    load_factor = 0.8
+    capacity = 16
 
     hash(key) {
         let hashCode = 0;
             
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
 
         return hashCode;
